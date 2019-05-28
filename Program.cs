a338dc7f2b7c018c5aaad4f68c0877772d42c418
SamBritt <sam.britt2@yahoo.com>
2019-05-28 11:39:21 -0500

    first commit using Lists

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace planetsAndSpaceShips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            // foreach (string planet in planetList)
+            // {
+            //     Console.WriteLine($"Planet: {planet}");
+            // }
+            
+            List<string> finalPlanets = new List<string>() { "Neptune", "Pluto" };
+            planetList.AddRange(finalPlanets);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Insert(6, "Uranus");
+            //S**t! Pluto apparently isn't a planet anymore!
+            planetList.Remove("Pluto");
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine($"Planet: {planet}");
+            }
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            foreach(string rockyPlanet in rockyPlanets){
+                Console.WriteLine($"Rocky Planet: {rockyPlanet}");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
